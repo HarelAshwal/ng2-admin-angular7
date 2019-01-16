db30a0e92196ac182d63fac310ea865fab520717
harel.ashwal <harel.ashwal@OPSL80008009>
2019-01-16 09:47:41 +0200

    upgrade to angular 7

--- src/app/app.translation.module.ts
+++ src/app/app.translation.module.ts
@@ -1,24 +1,28 @@
 import { NgModule } from '@angular/core';
-import { Http, HttpModule } from '@angular/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
 import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 import { TranslateService } from '@ngx-translate/core';
 
-export function createTranslateLoader(http: Http) {
-    return new TranslateHttpLoader(http, './assets/i18n/US/', '.json');
+export function createTranslateLoader(http: HttpClient) {
+  console.log(http);
+  return new TranslateHttpLoader(http, './assets/i18n/US/', '.json');
 }
 
 const translationOptions = {
   loader: {
     provide: TranslateLoader,
     useFactory: (createTranslateLoader),
-    deps: [Http]
+    deps: [HttpClient]
   }
 };
 
 @NgModule({
-  imports: [TranslateModule.forRoot(translationOptions)],
+  imports: [
+    // BrowserModule,
+    // HttpClientModule,
+    TranslateModule.forRoot(translationOptions)],
   exports: [TranslateModule],
   providers: [TranslateService]
 })
--- src/app/theme/nga.module.ts
+++ src/app/theme/nga.module.ts
@@ -1,8 +1,8 @@
-import { NgModule, ModuleWithProviders }      from '@angular/core';
-import { CommonModule }  from '@angular/common';
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { NgUploaderModule } from 'ngx-uploader';
+import { NgxUploaderModule } from 'ngx-uploader';
 import { AppTranslationModule } from '../app.translation.module';
 
 import {
@@ -112,7 +112,7 @@ const NGA_VALIDATORS = [
     FormsModule,
     ReactiveFormsModule,
     AppTranslationModule,
-    NgUploaderModule
+    NgxUploaderModule
   ],
   exports: [
     ...NGA_PIPES,
@@ -122,7 +122,7 @@ const NGA_VALIDATORS = [
 })
 export class NgaModule {
   static forRoot(): ModuleWithProviders {
-    return <ModuleWithProviders> {
+    return <ModuleWithProviders>{
       ngModule: NgaModule,
       providers: [
         BaThemeConfigProvider,
